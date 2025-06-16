9237eb8ca4181c04b292ab29f0594207c0d857f7
atalibaneto <atalibaneto@gmail.com>
2025-06-16 11:54:56 -0300

    Aula 16/06/2025 - atribuição, ++, --, %, **

--- DesenvolveWEB/php/aula1/index.php
+++ DesenvolveWEB/php/aula1/index.php
@@ -21,18 +21,18 @@
     $texto = "<h1>Sou aluno da TI36 no Senac de Botucatu</h1>";
     echo $texto;
 
-    $divisao = "<br>==============================================================<br>";
-    echo $divisao;
+    $traco = "<br>==============================================================<br>";
+    echo $traco;
     $nome = "Chaves";
     $sobrenome = "del Ocho";
     echo "Olá, meu nome é $nome e meu sobrenome é $sobrenome.";
-    echo $divisao;
+    echo $traco;
     /* Ex. 1 - Crie um script PHP que declare três variáveis: $nome, $idade e $cidade. Atribua valores a essas variávei e exiba uma mensagem que combine esses valores. */
     $nome = "Ataliba";
     $idade = "28";
     $cidade = "Botucatu";
     echo "Meu nome é $nome tenho $idade de CNH e moro em $cidade";
-    echo $divisao;
+    echo $traco;
     /* 
     Operações Aritméticas no PHP:
     Soma:          +
@@ -46,8 +46,19 @@
     $soma = $numero1 + $numero2;
     echo "A soma entre $numero1 e $numero2 é igual a $soma";
 
-    $divisao;
+    echo $traco;
     /* Ex.2 - Crie um script PHP que declare duas variáveis númericas, $num1 e $num2. Realize as operações de soma, subtração, multiplicação e divisão entre essas variáveis e exiba os resultados. */
+    $num1 = 7252;
+    $num2 = 98;
+    $adicao = $num1 + $num2;
+    $subtracao = $num1 - $num2;
+    $multiplicacao = $num1 * $num2;
+    $divisao = $num1 / $num2;
+    echo "A soma entre os números é: $adicao<br>";
+    echo "A subtração entre os números é: $subtracao<br>";
+    echo "A multiplicação entre os números é: $multiplicacao<br>";
+    echo "A divisão entre os números é: $divisao";
+    echo $traco;
     ?>
 </body>
 
--- DesenvolveWEB/php/index.php
+++
@@ -1,1 +0,0 @@
-<? echo "Olá mundo!"; ?>
